5baeadb2a2eab5fcff69800cafadb3833a02e645
Matthew Emond <memond93@gmail.com>
2015-10-08 19:27:19 -0400

    Convert tabs to spaces

--- core/models.py
+++ core/models.py
@@ -1,27 +1,30 @@
 from django.contrib.auth.models import User
 from django.db import models
 
-class UserProfile(models.Model):
-	"""An extension of Django's default user model. 
 
-	This allows for the storage of extra information about a user.
-	"""
-	user = models.OneToOneField(User)
-	movies = models.ManyToManyField('Movie', related_name='Users')
+class UserProfile(models.Model):
+    """An extension of Django's default user model.
+
+    This allows for the storage of extra information about a user.
+    """
+    user = models.OneToOneField(User)
+    movies = models.ManyToManyField('Movie', related_name='Users')
+
 
 class Movie(models.Model):
-	"""A movie seen by a user.
+    """A movie seen by a user.
 
-	Relates to `models.UserProfile` and `models.Actor`.
-	"""
-	name = models.CharField(max_length=255)
-	imdb_id = models.IntegerField(null=False)
-	actors = models.ManyToManyField('Actor', related_name='movies')
+    Relates to `models.UserProfile` and `models.Actor`.
+    """
+    name = models.CharField(max_length=255)
+    imdb_id = models.IntegerField(null=False)
+    actors = models.ManyToManyField('Actor', related_name='movies')
+
 
 class Actor(models.Model):
-	"""An actor in a movie.
+    """An actor in a movie.
 
-	Relates to `models.Movie`.
-	"""
-	name = models.CharField(max_length=255)
-	imdb_id = models.IntegerField(null=False)
+    Relates to `models.Movie`.
+    """
+    name = models.CharField(max_length=255)
+    imdb_id = models.IntegerField(null=False)
