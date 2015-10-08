11e1b4fa91c241e6632d4e748f5fbc7085323aa0
Jesse McBride <mcbjesse@gmail.com>
2015-10-08 19:05:44 -0400

    Adding some models.

--- core/models.py
+++ core/models.py
@@ -1,3 +1,27 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+class UserProfile(models.Model):
+	"""An extension of Django's default user model. 
+
+	This allows for the storage of extra information about a user.
+	"""
+	user = models.OneToOneField(User)
+	movies = models.ManyToManyField('Movie', related_name='Users')
+
+class Movie(models.Model):
+	"""A movie seen by a user.
+
+	Relates to `models.UserProfile` and `models.Actor`.
+	"""
+	name = models.CharField(max_length=255)
+	imdb_id = models.IntegerField(null=False)
+	actors = models.ManyToManyField('Actor', related_name='movies')
+
+class Actor(models.Model):
+	"""An actor in a movie.
+
+	Relates to `models.Movie`.
+	"""
+	name = models.CharField(max_length=255)
+	imdb_id = models.IntegerField(null=False)
