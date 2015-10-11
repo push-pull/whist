ce82703da9e3bd06f7f1d153d58f686c074458b6
Matthew Emond <memond93@gmail.com>
2015-10-11 04:05:19 -0400

    Moved whist title to base.html. Moved search bar to its own file. Added basic movie page.

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,8 @@
 from django.conf.urls import url, patterns
 
-# from core import views
-
 urlpatterns = patterns(
     'core.views',
     url(r'^$', 'index', name="index"),
     url(r'^search/$', 'search', name="search"),
+    url(r'^movie/(?P<movie_id>[0-9]+)/$', 'movie', name="movie"),
 )
--- core/views.py
+++ core/views.py
@@ -1,12 +1,25 @@
-# from django.http import HttpResponse
 from django.shortcuts import render
 
 from imdb import IMDb
 
 imdb = IMDb()
 
+
 def index(request):
     return render(request, 'index.html')
+
+
+def movie(request, movie_id):
+
+    movie = imdb.get_movie(movie_id)
+
+    return render(
+        request,
+        'movie.html',
+        {
+            'movie': movie,
+        }
+    )
 
 
 def search(request):
